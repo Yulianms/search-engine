334be167aa79e536a0c2e60dc8f0c02d3faa5582
Yulian Martinez <yulianms06@gmail.com>
2025-01-12 01:39:22 +0100

    Initial commit

---
+++ src/main/java/cards/poketracker/searchengine/SearchEngineApplication.java
@@ -0,0 +1,13 @@
+package cards.poketracker.searchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchEngineApplication.class, args);
+    }
+
+}
